cfdf744410d64b07da062b9c7e9be06d693b4a96
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2023-05-27 18:50:00 +0200

    add condition

--- x11_load.sh
+++ x11_load.sh
@@ -3,6 +3,8 @@
 TMP_DIR=/tmp/x11/
 U=$(logname)
 
-export DISPLAY="$(cat $TMP_DIR"/"$U.display)"
-xauth add $(cat $TMP_DIR"/"$U.xauth)
-
+if [ -a "$TMP_DIR"/"$U.display" ] && [ -a $TMP_DIR"/"$U.xauth ]
+  then
+  export DISPLAY="$(cat $TMP_DIR"/"$U.display)"
+  xauth add $(cat $TMP_DIR"/"$U.xauth)
+  fi
