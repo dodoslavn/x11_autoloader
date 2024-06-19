4f96ae62aa00d5127008331cbfd6d93a8ae33a69
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-06-19 23:00:28 +0200

    Update x11_save.sh

--- scripts/x11_save.sh
+++ scripts/x11_save.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+# script to save the X11 configuration to file
 
-TMP_DIR=/tmp/x11/
-mkdir -p $TMP_DIR
+# move to directory of this script
+cd $(dirname "$( realpath "$0" )")
+
+# check if config exists
+if ! [ -a "./config.sh" ]
+  then
+  echo "ERROR: Config file was not found!"
+  exit 2
+  fi
+
+# load shared variables
+source ./config.sh
+
+# check if DISPLAY env variable is set
+# if not, its ok. User just didnt enable X11 on client side
+if [ "$DISPLAY" == "" ]
+  then
+  exit 0
+  fi
 
 echo $DISPLAY > $TMP_DIR"/"$USER.display
-
 xauth list | grep "/unix:"$( echo $DISPLAY | cut -d':' -f2 | cut -d'.' -f1 ) > $TMP_DIR"/"$USER.xauth
-
